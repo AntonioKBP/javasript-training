e6d499e781ebde8c66fbdabdb6c689f50cdc9da4
Anton Kovsh <kovsh_anton@ukr.net>
2022-08-18 18:36:10 +0300

    Lesson 6 finished

---
+++ js/main.js
@@ -0,0 +1,131 @@
+// console.log("JavaScript says hello")
+//         console.info("JavaScript says hello")
+//         console.error("JavaScript says hello")
+//         console.warn("JavaScript says hello")
+
+// var num;
+// num = 5;
+
+// var num = 5;
+// num = 7;
+// console.log("Variables " + num + " check");
+
+//  next lesson
+
+//    first try
+// var num_1 = 5;
+// var num_2 = 15;
+// console.log("minus " + num_1 - num_2); NaN not a number
+
+//    second try works fine
+
+// var num_1 = 5;
+// var num_2 = 15;
+// var res = num_1 - num_2;
+// console.log("minus " + res);
+
+// third try and ow to do
+
+// var num_1 = 5;
+// var num_2 = 15;
+// var res = num_1 - num_2;
+// console.log("minus " + (num_1 - num_2));
+
+//  another examples
+
+// var num_1 = 5;
+// var num_2 = 15;
+// console.log("add " + (num_1 + num_2));
+
+// var num_1 = 5;
+// var num_2 = 15;
+// console.log("multiple " + (num_1 * num_2));
+
+// var num_1 = 5;
+// var num_2 = 15;
+// console.log("divide " + (num_1 / num_2));
+
+//  doing things
+
+// var num_3 = 5;
+// num_3 = num_3 + 7;
+// num_3 += 7; =12
+// num_3 *= 7; =35
+// num_3 /= 7; =0.7
+
+// num_3++; ++ add 1 total = 6; -- minus 1 total = 4;
+
+// console.log("result " + num_3);
+
+// var str_1 = "12";
+// var str_2 = "2";
+
+// console.log("result " + (str_1 + str_2));  stroke add to each other total 122
+
+// var str_1 = Number("12");
+// var str_2 = Number("2");
+
+// console.log("result " + (str_1 + str_2)); add Number made good even stroke multiplies to total 14
+
+//    Math examples
+
+// console.log("Math " + Math.PI); 3.141592653589793;
+
+// console.log("Math " + Math.sin(1));
+
+// console.log("Math " + Math.max(0, 3, 7, 9, 5)); 9
+
+// console.log("Math " + Math.min(1, 3, 7, 9, 5)); 1
+
+//    Lesson 6
+
+// var number = 15;
+
+// if (number == 5) {
+// 	console.log("Ok!");
+// } else if (number < 10) {
+// 	console.log("Okeysi");
+// } else if (number == 7) {
+// 	console.log("7");
+// } else if (number >= 5) {
+// 	console.log(">=15");
+// } else {
+// 	console.log("Error");
+// }                         total ">=15" win))
+
+// var number = 15;
+// var isHasHouse = true;
+
+// if (number == 5 || isHasHouse == true) {              || - or
+// 	console.log("Ok!");                                  && - and
+// } else if (number < 10) {                             isHasHouse without =true is ok in this ex
+// 	console.log("Okeysi");                               isHasHouse == false may write !isHasHouse ( !-not isHasHouse)
+// } else if (number == 7) {
+// 	console.log("7");
+// } else if (number >= 5) {
+// 	console.log(">=15");
+// } else {
+// 	console.log("Error");
+// }
+
+// var stroke = "example";
+
+// switch (stroke) {
+// 	case "4":
+// 		console.log("variables 4");
+// 		break;
+// 	case "45":
+// 		console.log("variables 45");
+// 		break;
+// 	case "example":
+// 		console.log("variables example found");
+// 		break;
+//     default:
+//         console.log("default");
+//         break;
+// }
+
+//  Lesson 7
+
+var lesson7 = 7;
+console.log("Lets Go");
