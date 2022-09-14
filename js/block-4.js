79633fccfcc5610b6b24e6a9e9b174aad49083d8
Anton Kovsh <kovsh_anton@ukr.net>
2022-09-14 15:13:07 +0300

    upd 21

--- js/block-4.js
+++ js/block-4.js
@@ -207,27 +207,29 @@
 // 	а создавала, наполняла и возвращала новый массив с
 // 		обновлёнными значениями.
 
-function changeEven(numbers, value) {
-	// Change code below this line
-	const newArray = [];
-	// console.log(newArray);
-	numbers.forEach(element => {
-		if (element % 2 === 0) {
-			newArray.push(element + value);
-			// console.log(newArray);
-		}
-	});
-	return newArray;
+// function changeEven(numbers, value) {
+// 	// Change code below this line
+// 	const newArray = [];
 
-	// for (let i = 0; i < numbers.length; i += 1) {
-	// 	if (numbers[i] % 2 === 0) {
-	// 		numbers[i] = numbers[i] + value;
-	// 		console.log(numbers[i]);
-	// 	}
-	// }
-	// Change code above this line
-}
-console.log(changeEven([1, 2, 3, 4, 5], 10));
+// 	numbers.forEach(element => {
+// 		if (element % 2 === 0) {
+// 			newArray.push(element + value);
+// 		} else {
+// 			newArray.push(element);
+// 		}
+// 		// newArray.push(element);
+// 	});
+// 	return newArray;
+
+// 	// for (let i = 0; i < numbers.length; i += 1) {
+// 	// 	if (numbers[i] % 2 === 0) {
+// 	// 		numbers[i] = numbers[i] + value;
+// 	// 		console.log(numbers[i]);
+// 	// 	}
+// 	// }
+// 	// Change code above this line
+// }
+// console.log(changeEven([1, 2, 3, 4, 5], 10));
 
 // Вызов changeEven([1, 2, 3, 4, 5], 10)
 // возвращает новый массив[1, 12, 3, 14, 5]
@@ -237,6 +239,157 @@ console.log(changeEven([1, 2, 3, 4, 5], 10));
 // возвращает новый массив[17, 124, 168, 31, 142]
 // Вызов changeEven([44, 13, 81, 92, 36, 54], 100)
 // возвращает новый массив[144, 13, 81, 192, 136, 154]
+
+// ========================= 14 ==============================
+
+// Дополни код так, чтобы в переменной planetsLengths получился массив длин
+// названий планет.Обязательно используй метод map().
+
+//     Объявлена переменная planets
+//     Значение переменной planets это массив ["Earth", "Mars", "Venus", "Jupiter"]
+//     Объявлена переменная planetsLengths
+//     Значение переменной planetsLengths это массив [5, 4, 5, 7]
+//     Для перебора массива планет использован метод map()
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map(planet => planet.length);
+
+// console.log(planetsLengths);
+// ========================= 17 ==============================
+// const users = [
+// 	{
+// 		name: "Moore Hensley",
+// 		email: "moorehensley@indexia.com",
+// 		eyeColor: "blue",
+// 		friends: ["Sharron Pace"],
+// 		isActive: false,
+// 		balance: 2811,
+// 		skills: ["ipsum", "lorem"],
+// 		gender: "male",
+// 		age: 37,
+// 	},
+// 	{
+// 		name: "Sharlene Bush",
+// 		email: "sharlenebush@tubesys.com",
+// 		eyeColor: "blue",
+// 		friends: ["Briana Decker", "Sharron Pace"],
+// 		isActive: true,
+// 		balance: 3821,
+// 		skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+// 		gender: "female",
+// 		age: 34,
+// 	},
+// 	{
+// 		name: "Ross Vazquez",
+// 		email: "rossvazquez@xinware.com",
+// 		eyeColor: "green",
+// 		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+// 		isActive: false,
+// 		balance: 3793,
+// 		skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+// 		gender: "male",
+// 		age: 24,
+// 	},
+// 	{
+// 		name: "Elma Head",
+// 		email: "elmahead@omatom.com",
+// 		eyeColor: "green",
+// 		friends: ["Goldie Gentry", "Aisha Tran"],
+// 		isActive: true,
+// 		balance: 2278,
+// 		skills: ["adipisicing", "irure", "velit"],
+// 		gender: "female",
+// 		age: 21,
+// 	},
+// 	{
+// 		name: "Carey Barr",
+// 		email: "careybarr@nurali.com",
+// 		eyeColor: "blue",
+// 		friends: ["Jordan Sampson", "Eddie Strong"],
+// 		isActive: true,
+// 		balance: 3951,
+// 		skills: ["ex", "culpa", "nostrud"],
+// 		gender: "male",
+// 		age: 27,
+// 	},
+// 	{
+// 		name: "Blackburn Dotson",
+// 		email: "blackburndotson@furnigeer.com",
+// 		eyeColor: "brown",
+// 		friends: ["Jacklyn Lucas", "Linda Chapman"],
+// 		isActive: false,
+// 		balance: 1498,
+// 		skills: ["non", "amet", "ipsum"],
+// 		gender: "male",
+// 		age: 38,
+// 	},
+// 	{
+// 		name: "Sheree Anthony",
+// 		email: "shereeanthony@kog.com",
+// 		eyeColor: "brown",
+// 		friends: ["Goldie Gentry", "Briana Decker"],
+// 		isActive: true,
+// 		balance: 2764,
+// 		skills: ["lorem", "veniam", "culpa"],
+// 		gender: "female",
+// 		age: 39,
+// 	},
+// ];
+
+// const getUserNames = users => {
+// 	return users.map(user => user.name);
+// 	console.log(user);
+// };
+// console.log(getUserNames);
+// console.log(users);
+
+// ========================= 19 =============================
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 === 1);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+// ========================= 20 =============================
+
+const books = [
+	{
+		title: "The Last Kingdom",
+		author: "Bernard Cornwell",
+		genres: ["adventure", "history"],
+	},
+	{
+		title: "Beside Still Waters",
+		author: "Robert Sheckley",
+		genres: ["fiction", "mysticism"],
+	},
+	{
+		title: "Redder Than Blood",
+		author: "Tanith Lee",
+		genres: ["horror", "mysticism", "adventure"],
+	},
+];
+// Change code below this line
+const allGenres = books.flatMap(book => book.genres);
+const uniqueGenres = allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
+console.log(allGenres);
+console.log(uniqueGenres);
 // ========================= # ==============================
+
 // ========================= # ==============================
+
+// ========================= # ==============================
+
+// ========================= # ==============================
+
+// ========================= # ==============================
+
+// ========================= # ==============================
+
+// ========================= # ==============================
+
 // ========================= # ==============================
