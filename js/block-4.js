db477f8e724b47714ce063637ebef096e00ea12b
Anton Kovsh <kovsh_anton@ukr.net>
2022-09-16 20:00:55 +0300

    upd 26

--- js/block-4.js
+++ js/block-4.js
@@ -378,7 +378,253 @@
 // const uniqueGenres = allGenres.filter((genre, index, array) => array.indexOf(genre) === index);
 // console.log(allGenres);
 // console.log(uniqueGenres);
-// ========================= # ==============================
+// ========================= 21 ==============================
+
+// Используя метод filter() дополни код так, чтобы:
+
+//     В переменной topRatedBooks получился массив книг рейтинг которых(свойство rating)
+// 	 больше либо равно значению переменной MIN_RATING.
+//     В переменной booksByAuthor получился массив книг написанных автором с
+// именем(свойство author) которое совпадает со значением в переменной AUTHOR
+
+// const books = [
+// 	{
+// 		title: "The Last Kingdom",
+// 		author: "Bernard Cornwell",
+// 		rating: 8.38,
+// 	},
+// 	{
+// 		title: "Beside Still Waters",
+// 		author: "Robert Sheckley",
+// 		rating: 8.51,
+// 	},
+// 	{
+// 		title: "The Dream of a Ridiculous Man",
+// 		author: "Fyodor Dostoevsky",
+// 		rating: 7.75,
+// 	},
+// 	{ title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+// 	{ title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = "Bernard Cornwell";
+// // Change code below this line
+
+// const topRatedBooks = books.filter(book => {
+// 	return book.rating > MIN_RATING;
+// });
+// const booksByAuthor = books.filter(book => {
+// 	return book.author === AUTHOR;
+// });
+
+// console.log(topRatedBooks);
+// console.log(booksByAuthor);
+
+// Объявлена переменная books
+// Значение переменной books это массив объектов
+// Объявлена переменная MIN_RATING
+// Значение переменной MIN_RATING это число 8
+// Объявлена переменная AUTHOR
+// Значение переменной AUTHOR это строка "Bernard Cornwell"
+// Объявлена переменная topRatedBooks
+// Значение переменной topRatedBooks это массив книг с рейтингов выше 8
+// Объявлена переменная booksByAuthor
+// Значение переменной booksByAuthor это массив книг автор которых "Bernard Cornwell"
+// Для перебора массива books использован метод filter()
+
+// ========================= 24 ==============================
+
+// const users = [
+// 	{
+// 		name: "Moore Hensley",
+// 		email: "moorehensley@indexia.com",
+// 		eyeColor: "blue",
+// 		friends: ["Sharron Pace"],
+// 		isActive: false,
+// 		balance: 2811,
+// 		gender: "male",
+// 		age: 37,
+// 	},
+// 	{
+// 		name: "Sharlene Bush",
+// 		email: "sharlenebush@tubesys.com",
+// 		eyeColor: "blue",
+// 		friends: ["Briana Decker", "Sharron Pace"],
+// 		isActive: true,
+// 		balance: 3821,
+// 		gender: "female",
+// 		age: 34,
+// 	},
+// 	{
+// 		name: "Ross Vazquez",
+// 		email: "rossvazquez@xinware.com",
+// 		eyeColor: "green",
+// 		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+// 		isActive: false,
+// 		balance: 3793,
+// 		gender: "male",
+// 		age: 24,
+// 	},
+// 	{
+// 		name: "Elma Head",
+// 		email: "elmahead@omatom.com",
+// 		eyeColor: "green",
+// 		friends: ["Goldie Gentry", "Aisha Tran"],
+// 		isActive: true,
+// 		balance: 2278,
+// 		gender: "female",
+// 		age: 21,
+// 	},
+// 	{
+// 		name: "Carey Barr",
+// 		email: "careybarr@nurali.com",
+// 		eyeColor: "blue",
+// 		friends: ["Jordan Sampson", "Eddie Strong"],
+// 		isActive: true,
+// 		balance: 3951,
+// 		gender: "male",
+// 		age: 27,
+// 	},
+// 	{
+// 		name: "Blackburn Dotson",
+// 		email: "blackburndotson@furnigeer.com",
+// 		eyeColor: "brown",
+// 		friends: ["Jacklyn Lucas", "Linda Chapman"],
+// 		isActive: false,
+// 		balance: 1498,
+// 		gender: "male",
+// 		age: 38,
+// 	},
+// 	{
+// 		name: "Sheree Anthony",
+// 		email: "shereeanthony@kog.com",
+// 		eyeColor: "brown",
+// 		friends: ["Goldie Gentry", "Briana Decker"],
+// 		isActive: true,
+// 		balance: 2764,
+// 		gender: "female",
+// 		age: 39,
+// 	},
+// ];
+// // console.log(users);
+// const friendName = "Briana Decker";
+// const getUsersWithFriend = (users, friendName) => {
+// 	console.log(users);
+// 	return users.filter(({ friends }) => {
+// 		if (friends.includes(friendName)) {
+// 			return friends;
+// 		}
+// 	});
+// };
+
+// console.log(getUsersWithFriend(users, "Briana Decker"));
+// console.log(getUsersWithFriend(users, "Goldie Gentry"));
+
+// Дополни функцию getUsersWithFriend(users, friendName) так,
+// чтобы она возвращала массив пользователей у которых есть друг с именем в параметре friendName.
+//  Массив друзей пользователя хранится в свойстве friends.
+
+// Change code below this line
+
+// Change code above this line
+
+// Объявлена переменная getUsersWithFriend
+// Переменной getUsersWithFriend присвоена стрелочная функция с параметрами (users, friendName)
+// Для перебора параметра users используется метод filter()
+// Если значение параметра friendName это строка "Briana Decker",
+// функция возвращает массив объектов пользователей с именами Sharlene Bush и Sheree Anthony
+// Если значение параметра friendName это строка "Goldie Gentry",
+// функция возвращает массив объектов пользователей с именами Elma Head и Sheree Anthony
+// Если значение параметра friendName это строка "Adrian Cross",
+// функция возвращает пустой массив
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+// ========================= 25 =============================
+const users = [
+	{
+		name: "Moore Hensley",
+		email: "moorehensley@indexia.com",
+		eyeColor: "blue",
+		friends: ["Sharron Pace"],
+		isActive: false,
+		balance: 2811,
+		gender: "male",
+		age: 37,
+	},
+	{
+		name: "Sharlene Bush",
+		email: "sharlenebush@tubesys.com",
+		eyeColor: "blue",
+		friends: ["Briana Decker", "Sharron Pace"],
+		isActive: true,
+		balance: 3821,
+		gender: "female",
+		age: 34,
+	},
+	{
+		name: "Ross Vazquez",
+		email: "rossvazquez@xinware.com",
+		eyeColor: "green",
+		friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+		isActive: false,
+		balance: 3793,
+		gender: "male",
+		age: 24,
+	},
+	{
+		name: "Elma Head",
+		email: "elmahead@omatom.com",
+		eyeColor: "green",
+		friends: ["Goldie Gentry", "Aisha Tran"],
+		isActive: true,
+		balance: 2278,
+		gender: "female",
+		age: 21,
+	},
+	{
+		name: "Carey Barr",
+		email: "careybarr@nurali.com",
+		eyeColor: "blue",
+		friends: ["Jordan Sampson", "Eddie Strong"],
+		isActive: true,
+		balance: 3951,
+		gender: "male",
+		age: 27,
+	},
+	{
+		name: "Blackburn Dotson",
+		email: "blackburndotson@furnigeer.com",
+		eyeColor: "brown",
+		friends: ["Jacklyn Lucas", "Linda Chapman"],
+		isActive: false,
+		balance: 1498,
+		gender: "male",
+		age: 38,
+	},
+	{
+		name: "Sheree Anthony",
+		email: "shereeanthony@kog.com",
+		eyeColor: "brown",
+		friends: ["Goldie Gentry", "Briana Decker"],
+		isActive: true,
+		balance: 2764,
+		gender: "female",
+		age: 39,
+	},
+];
+// console.log(users);
+// Дополни функцию getFriends(users) так, чтобы она возвращала массив друзей всех
+// пользователей(свойство friends).У нескольких пользователей могут быть одинаковые друзья,
+// сделай так чтобы возвращаемый массив не содержал повторений.
+const getFriends = users => {
+	const allUserFriends = users.flatMap(user => user.friends);
+	return allUserFriends.filter((friend, idx, arr) => arr.indexOf(friend) === idx);
+};
+console.log(getFriends(users));
+// Вызов функции с указанным массивом пользователей возвращает массив["Sharron Pace", "Briana Decker",
+// 	"Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner", "Goldie Gentry", "Aisha Tran", "Jordan Sampson",
+// 	"Eddie Strong", "Jacklyn Lucas", "Linda Chapman"]
 
 // ========================= # ==============================
 
@@ -389,11 +635,7 @@
 // ========================= # ==============================
 
 // ========================= # ==============================
-
-// ========================= # ==============================
-
-// ========================= # ==============================
-const a = [1, 8, 2, 3, 42, 7, 4];
+// const a = [1, 8, 2, 3, 42, 7, 4];
 // max = a[0];
 // for (let i = 0; i < a.length; i += 1) {
 // 	if (a[i] > max) {
@@ -423,13 +665,13 @@ const a = [1, 8, 2, 3, 42, 7, 4];
 // });
 // console.log(flat);
 
-const data = [
-	[1, 2, 3],
-	[3, 4, 5],
-	[5, 6, 7],
-];
-const flat = data.reduce((prev, item) => {
-	newArray = [];
-	newArray.push(...item);
-});
-console.log(newArray);
+// const data = [
+// 	[1, 2, 3],
+// 	[3, 4, 5],
+// 	[5, 6, 7],
+// ];
+// const flat = data.reduce((prev, item) => {
+// 	newArray = [];
+// 	newArray.push(...item);
+// });
+// console.log(newArray);
