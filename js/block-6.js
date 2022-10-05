c5003d25fa107cb93fbe4ada6f743a7b623c1fdc
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-05 13:49:18 +0300

    upd

--- js/block-6.js
+++ js/block-6.js
@@ -1,6 +1,39 @@
 // ==============================================================================================
 
 // =================================== 1 ====================================
+// const imageEl = document.createElement("img");
+// imageEl.src = "https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg";
+// imageEl.alt = "valais-alpine-mountains-glacier";
+// imageEl.width = 320;
+// // console.log('imageEl', imageEl);
+
+// // document.body.appendChild(imageEl);
+
+// const heroEl = document.querySelector(".hero");
+// // heroEl.appendChild(titleEl);
+// // heroEl.appendChild(imageEl);
+// heroEl.append(titleEl, imageEl);
+
+/*
+ * Создаём и добавляем новый пункт меню
+ */
+const navItemEl = document.createElement("li");
+navItemEl.classList.add("site-nav__item");
+console.log(navItemEl);
+
+const navLinkEl = document.createElement("a");
+navLinkEl.classList.add("site-nav__link");
+navLinkEl.textContent = "Личный кабинет";
+navLinkEl.href = "/profile";
+console.log(navLinkEl);
+
+// navItemEl.appendChild(navLinkEl);
+// // console.log(navItemEl);
+
+// const navEl = document.querySelector(".site-nav");
+
+// // navEl.appendChild(navItemEl);
+// navEl.insertBefore(navItemEl, navEl.firstElementChild);
 
 // =================================== 2 ====================================
 
